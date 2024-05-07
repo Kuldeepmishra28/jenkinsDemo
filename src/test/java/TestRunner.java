01d808fb610fad9872b5ef239b0837ea56daa162
kuldeepmishra <kuldeep.mishra@nagarro.com>
2024-05-07 15:12:57 +0530

    commit

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,14 @@
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		glue = "StepDefinitions",
+		features = "src/test/java/Features",
+		plugin= {"pretty", "html:testReport.html"},
+		tags="@cart")
+public class TestRunner {
+
+}
